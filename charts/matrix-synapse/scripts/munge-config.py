d1f356722ea809abf38f7ae36138cd5be7338dc1
Alexander Olofsson <ace@haxalot.com>
2024-08-10 21:31:15 +0200

    Convert old secrets handling to env-vars for Synapse

--- charts/matrix-synapse/scripts/munge-config.py
+++ charts/matrix-synapse/scripts/munge-config.py
@@ -1,20 +1,34 @@
 #!/bin/env python
 
-import glob
 import os
 import re
+import sys
 import yaml
 
 
 def expand_macro(inp, fulldict):
+    """
+    Expands macros in the configuration, supported syntax is;
+    %{env:ENVIRONMENT_VARIABLE}%
+    %{file:/path/to/file}%
+    %{cfg:path.to.config}%
+
+    Adding a ! before the macro name will remove the key from the configuration
+    if it is missing.
+    """
     key, data = inp
+    remove_key = False
     if type(data) is dict:
         return key, expand_macros(data, fulldict)
     elif type(data) is str:
         newdata = data
         for macro in re.finditer(r"%{([^:]+):([^}]+)}%?", data):
             repl = ""
+            remove_if_missing = False
             method, data = macro[1], macro[2]
+            if method[0] == '!':
+                remove_if_missing = True
+                method = method[1:]
             match method:
                 case 'env':
                     try:
@@ -38,8 +52,13 @@ def expand_macro(inp, fulldict):
                         print(f"Failed to read config key {data} - {e}")
                         value = ''
                     repl = value
-            newdata = newdata.replace(macro[0], repl)
+            if remove_if_missing and not repl:
+                remove_key = True
+            else:
+                newdata = newdata.replace(macro[0], repl)
         data = newdata
+    if remove_key:
+        return None
     return key, data
 
 
@@ -52,14 +71,22 @@ def expand_macros(inp, fulldict=None):
 
 
 data = {}
-with open('/synapse/config/homeserver.yaml', 'r') as file:
-    data.update(yaml.safe_load(file))
-for filename in glob.glob('/synapse/secrets/*.ymal'):
-    with open(filename, 'r') as file:
+if len(sys.argv) > 1:
+    for filename in sys.argv[1:]:
+        with open(filename, 'r') as file:
+            data.update(yaml.safe_load(file))
+else:
+    with open('/synapse/config/homeserver.yaml', 'r') as file:
         data.update(yaml.safe_load(file))
-for filename in glob.glob('/synapse/config/conf.d/*.ymal'):
-    with open(filename, 'r') as file:
-        data.update(yaml.safe_load(file))
+    try:
+        for filename in os.listdir('/synapse/secrets'):
+            if not re.match(r"\.ya?ml", filename):
+                continue
+            with open(os.path.join('/synapse/secrets', filename), 'r') as file:
+                data.update(yaml.safe_load(file))
+    except FileNotFoundError:
+        # No extra secrets provided, ignoring.
+        pass
 
 expanded = expand_macros(data)
 print(yaml.dump(expanded))
